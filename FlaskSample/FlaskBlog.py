3c0f0092209264e076c0a7c5a610754608a2c24a
WeiHon <c.weihon@gmail.com>
2018-09-28 02:12:59 +0800

    Flask_Lesson#2: Templates

--- FlaskSample/FlaskBlog.py
+++ FlaskSample/FlaskBlog.py
@@ -1,15 +1,29 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Chong Wei Hon',
+        'title': 'Blog Post 1',
+        'content': '1st Post Content',
+        'date_posted': 'Sept 28, 2018'
+    },
+    {
+        'author': 'Wong Fun Ying',
+        'title': 'Blog Post 2',
+        'content': '2nd Post Content',
+        'date_posted': 'Aug 28, 2018'
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Hello Python3 Home!</h1>"
+    return render_template('home.html', posts=posts, title='Main')
 
 @app.route("/about")
-
 def About():
-    return "<h1>Hello Python3 About Page!</h1>"
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(debug=True)
