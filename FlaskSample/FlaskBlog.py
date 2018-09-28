a27ecec99b5c6cc1cda2b5e6c9754d1601aee28d
WeiHon <c.weihon@gmail.com>
2018-09-29 00:42:09 +0800

    Flask_Lesson#2: Templates

--- FlaskSample/FlaskBlog.py
+++ FlaskSample/FlaskBlog.py
@@ -1,5 +1,9 @@
 from flask import Flask, render_template, url_for
+from form import RegistrationForm, LoginForm
+
 app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '20ebda5dea90a24375719c286881ea98'
 
 posts = [
     {
@@ -22,8 +26,18 @@ def home():
     return render_template('home.html', posts=posts, title='Main')
 
 @app.route("/about")
-def About():
+def about():
     return render_template('about.html', title='About')
+
+@app.route("/register")
+def register():
+    form = RegistrationForm()
+    return render_template('register.html', title='Registration', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
