6fb96780c08ae438208aacc753297a460961cbde
WeiHon <c.weihon@gmail.com>
2018-10-05 01:42:07 +0800

    Flask_Lesson#4: Create Sqlite DB from Python

--- FlaskSample/FlaskBlog/Cmd.py
+++ FlaskSample/FlaskBlog/Cmd.py
@@ -2,7 +2,7 @@
 - Command to create DB file as per FlaskBlog mention
 - Sqlite DB file will be create in same directory as source code
 '''
-from FlaskBlog import db
+from FlaskBlog.models import db, User, Post
 
 db.create_all()
 
@@ -10,3 +10,7 @@ db.create_all()
 - drop all tables
 '''
 #db.drop_all()
+
+'''
+get user list
+'''
---
+++ FlaskSample/FlaskBlog/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '20ebda5dea90a24375719c286881ea98'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
+from FlaskBlog import routes
